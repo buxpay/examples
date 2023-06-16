33b0e57bed06e4e56074cde269c6e220580fa59c
fsdfd <133547586+fsfsasdfs@users.noreply.github.com>
2023-06-16 23:36:04 +0300

    FIXED BOT!

--- robux_donation_bot.py
+++ robux_donation_bot.py
@@ -16,11 +16,11 @@ async def on_ready():
 
 @bot.tree.command(name="donate")
 async def donate(interaction: discord.Interaction, amount_of_robux: int):
-    await interaction.response.send_message("Creating gamepass...")
+    await interaction.response.send_message("Creating gamepass...",ephemeral=True)
     res = requests.post("https://buxpay.xyz/payments/create",json={"api_key":api_key,"price":int(amount_of_robux)}).json() # create an invoice
     gamepass = res["data"]["gamepass"] # gamepass id
     uid = res["data"]["uid"] #unique id for checking the status, etc.
-    await interaction.edit_original_response(f"Please purchase this gamepass: https://www.roblox.com/game-pass/{gamepass}/",ephemeral=True)
+    await interaction.edit_original_response(content=f"Please purchase this gamepass: https://www.roblox.com/game-pass/{gamepass}/")
 
     def check_if_bought():
       check = requests.get(f"https://buxpay.xyz/payments/info",json={"api_key":api_key,"uid":uid})
@@ -34,5 +34,6 @@ async def donate(interaction: discord.Interaction, amount_of_robux: int):
     else: # since they have bought the gamepass
         # do something here (eg. give them a role), i just made it say thank you for donating.
         return await interaction.followup.send("Thank you for donating to us!",ephemeral=True)
+
 
 bot.run(bot_token)
